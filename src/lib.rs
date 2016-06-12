1cfdc459f2a148f05bfa9e75c36215cbc7b0e266
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2016-06-12 12:17:23 -0300

    [FIX] Created init method with args

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
+#[macro_use]
+extern crate log;
 
+extern crate argparse;
+use argparse::{ArgumentParser, StoreTrue, Store};
+
+pub fn init() {
+	info!("Initialize");
+
+    let mut verbose = false;
+    let mut file = "app.ini".to_string();
+
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Parser generic data into JSON.");
+        ap.refer(&mut verbose).add_option(&["-v", "--verbose"], StoreTrue, "Show processing data");
+        ap.refer(&mut file).add_option(&["--config"], Store, "Config file name");
+        ap.parse_args_or_exit();
+    }
+
+	if verbose {
+        println!("file name is {}", file);
+    }
+    println!("Loading {}!", file);
+}
